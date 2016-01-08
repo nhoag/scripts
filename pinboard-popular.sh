a68c96e1837875eacc8fc8fc4a2800ab05648784
Nathaniel Hoag <info@nathanielhoag.com>
2016-01-07 20:49:33 -0500

    Implement simple exclusion filter

--- pinboard-popular.sh
+++ pinboard-popular.sh
@@ -8,24 +8,40 @@ function usage() {
     OPTIONS:
       -h        Show usage
       -a        Return all items (ignoring past trends)
+      -x        Exclude domain
 EOF
 exit
 }
 
 PB_ALL_DATA=0
+PB_EXCLUDE=''
 
-while getopts ":ha" OPTION; do
+while getopts ":hax:" OPTION; do
   case $OPTION in
     h) usage                  ;;
     a) PB_ALL_DATA=1          ;;
+    x) PB_EXCLUDE=$OPTARG     ;;
   esac
 done
+
+function exclude_domain() {
+  if [[ ! -z ${EXCLUDE_DOMAIN} ]]; then
+    grep -Ev "${EXCLUDE_DOMAIN}"
+  else
+    cat
+  fi
+}
 
 function pinboard_popular() {
   local PB_DATE=$(date '+%Y%m%d')
   local OUT_FILE_STEM="pinboard-popular_"
   local OUT_FILE_ID="${OUT_FILE_STEM}${PB_DATE}"
   local OUT_FILE="${TMPDIR}${OUT_FILE_ID}"
+  if [[ ! -z ${PB_EXCLUDE} ]]; then
+    EXCLUDE_DOMAIN="^http(s)?://(www\.)?${PB_EXCLUDE//\./\\.}"
+  else
+    EXCLUDE_DOMAIN=''
+  fi
   if [[ -f $OUT_FILE ]]; then
     local EXCLUDE_FILE="! -name ${OUT_FILE_ID}"
     local OUT_FILE='/dev/null'
@@ -45,7 +61,13 @@ function pinboard_popular() {
     | xmllint --html --xpath '//a/@href|//a/text()' - 2>/dev/null \
     | sed 's#href=#\'$'\n#g' \
     | grep http \
-    | sed 's#^"##;s#"#\'$'\n#' \
+    | sed -E 's#^"##' \
+    | exclude_domain \
+    | sed -E 's#"#\'$'\n#' \
+    | sed -E "s/&acirc;&#128;&#14(7|8);/-/g" \
+    | sed -E "s/&acirc;&#128;&#153;/'/g" \
+    | sed -E 's/&acirc;&#128;&#15(6|7);/"/g' \
+    | sed -E 's/&amp;/\&/g' \
     | awk \
       -v past_trends="${PAST_TRENDS}" \
       -v out_file="${OUT_FILE}" '
@@ -70,3 +92,4 @@ function pinboard_popular() {
 }
 
 pinboard_popular
+
